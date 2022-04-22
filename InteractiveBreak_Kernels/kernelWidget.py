ee1090156c264829c489607b1a418b6044e4e9f5
joachim greiner <joachim.greiner@universitaets-herzzentrum.de>
2022-04-22 13:47:27 +0200

    added absolute value, simplified small example

--- InteractiveBreak_Kernels/kernelWidget.py
+++ InteractiveBreak_Kernels/kernelWidget.py
@@ -3,22 +3,47 @@ from ipywidgets import widgets
 import matplotlib.pyplot as plt
 from scipy import ndimage
 from skimage import data
+from ipywidgets import Checkbox
 
 class KernelWidget():
-    def __init__(self, kernel_min=-6, kernel_max=6, small_example=True):
+    def __init__(self, kernel_min=-8, kernel_max=8, small_example=True):
+        self.box_absolute = Checkbox(False, description='absolute value')
+        self.box_absolute.observe(handler=self.convolve)
+
+        self.w00 = widgets.IntSlider(min=kernel_min, max=kernel_max, value=0)
+        self.w01 = widgets.IntSlider(min=kernel_min, max=kernel_max, value=0)
+        self.w02 = widgets.IntSlider(min=kernel_min, max=kernel_max, value=0)
+        self.w10 = widgets.IntSlider(min=kernel_min, max=kernel_max, value=0)
+        self.w11 = widgets.IntSlider(min=kernel_min, max=kernel_max, value=1)
+        self.w12 = widgets.IntSlider(min=kernel_min, max=kernel_max, value=0)
+        self.w20 = widgets.IntSlider(min=kernel_min, max=kernel_max, value=0)
+        self.w21 = widgets.IntSlider(min=kernel_min, max=kernel_max, value=0)
+        self.w22 = widgets.IntSlider(min=kernel_min, max=kernel_max, value=0)
+
+        self.w00.observe(handler=self.convolve)
+        self.w01.observe(handler=self.convolve)
+        self.w02.observe(handler=self.convolve)
+        self.w10.observe(handler=self.convolve)
+        self.w11.observe(handler=self.convolve)
+        self.w12.observe(handler=self.convolve)
+        self.w20.observe(handler=self.convolve)
+        self.w21.observe(handler=self.convolve)
+        self.w22.observe(handler=self.convolve)
+
+        self.kernel =  np.zeros((3,3), dtype=np.int8)
+        self.kernel[1, 1] = 1
+
+
+
         self.small_example = small_example
-        if small_example:
+        if self.small_example:
             self.image = np.ones((5, 5))
             self.image[2, :] = 3
         else:
             self.image = data.brick()
             self.image = self.image.astype(np.float32)
 
-        self.convolved_image = self.image
-
-        self.kernel =  np.zeros((3,3), dtype=np.int8)
-        self.kernel[1, 1] = 1
-
+        self.convolved_image = np.empty_like(self.image)
 
         self.axes = plt.figure(figsize=(9, 4), constrained_layout=True).subplot_mosaic(
             """
@@ -52,7 +77,7 @@ class KernelWidget():
         self.ax_right.xaxis.set_visible(False)
         self.ax_right.yaxis.set_visible(False)
         plt.title('Filtered Image')
-        if small_example:
+        if self.small_example:
             self.img_obj.set_clim(0, 1+np.max([np.max(self.image), np.max(self.convolved_image)]))
             self.original_img.set_clim(0, 1+np.max([np.max(self.image), np.max(self.convolved_image)]))
 
@@ -64,26 +89,6 @@ class KernelWidget():
             self.conv_text = self.create_text_annotation(self.convolved_image, axes=self.ax_right)
 
         # plt.title('Used 3x3 kernel')
-
-        self.w00 = widgets.IntSlider(min=kernel_min, max=kernel_max, value=0)
-        self.w01 = widgets.IntSlider(min=kernel_min, max=kernel_max, value=0)
-        self.w02 = widgets.IntSlider(min=kernel_min, max=kernel_max, value=0)
-        self.w10 = widgets.IntSlider(min=kernel_min, max=kernel_max, value=0)
-        self.w11 = widgets.IntSlider(min=kernel_min, max=kernel_max, value=1)
-        self.w12 = widgets.IntSlider(min=kernel_min, max=kernel_max, value=0)
-        self.w20 = widgets.IntSlider(min=kernel_min, max=kernel_max, value=0)
-        self.w21 = widgets.IntSlider(min=kernel_min, max=kernel_max, value=0)
-        self.w22 = widgets.IntSlider(min=kernel_min, max=kernel_max, value=0)
-
-        self.w00.observe(handler=self.convolve)
-        self.w01.observe(handler=self.convolve)
-        self.w02.observe(handler=self.convolve)
-        self.w10.observe(handler=self.convolve)
-        self.w11.observe(handler=self.convolve)
-        self.w12.observe(handler=self.convolve)
-        self.w20.observe(handler=self.convolve)
-        self.w21.observe(handler=self.convolve)
-        self.w22.observe(handler=self.convolve)
 
         if small_example:
             data_selection_options = ['random noise', 'vertical edges', 'horizontal edges']
@@ -99,11 +104,16 @@ class KernelWidget():
 
         self.filter_selection.observe(self.create_image_data, names='value')
 
+
+
+        #update right image
+        self.convolve()
+
         self.hbox_1 = widgets.HBox((self.w00, self.w01, self.w02))
         self.hbox_2 = widgets.HBox((self.w10, self.w11, self.w12))
         self.hbox_3 = widgets.HBox((self.w20, self.w21, self.w22))
 
-        self.vbox = widgets.VBox((self.filter_selection, self.hbox_1, self.hbox_2, self.hbox_3))
+        self.vbox = widgets.VBox((self.filter_selection, self.box_absolute, self.hbox_1, self.hbox_2, self.hbox_3))
 
         display(self.vbox)
 
@@ -124,21 +134,31 @@ class KernelWidget():
                 # self.image[:, 0] = 1
                 self.image[:, 2] = 3
                 # self.image[:, 4] = 1
+
         else:
             if change['new'] == 'cat':
                 self.image = data.cat()[:, :, 0]
             elif change['new'] == 'brick':
                 self.image = data.brick()
 
-            self.image = self.image.astype(np.float32)
+        self.image = self.image.astype(np.float32)
 
 
         self.convolve()
+
         self.ax_left.clear()
         self.ax_right.clear()
 
+
         self.original_img = self.ax_left.imshow(self.image, cmap='gray')
+        self.ax_left.set_title('Original Image')
         self.img_obj = self.ax_right.imshow(self.convolved_image, cmap='gray')
+        self.ax_right.set_title('Filtered Image')
+
+        if self.small_example:
+            self.original_text = self.create_text_annotation(self.image, axes=self.ax_left)
+            self.conv_text = self.create_text_annotation(self.convolved_image, axes=self.ax_right)
+
 
 
 
@@ -157,16 +177,16 @@ class KernelWidget():
             self.img_obj.autoscale()
         else:
             self.img_obj.set_clim(self.min_imgs(self.image, self.convolved_image),
-                                  1 + np.max([np.max(self.image), np.max(self.convolved_image)]))
+                                  1 + self.max_imgs(self.image, self.convolved_image))
             self.original_img.set_clim(self.min_imgs(self.image, self.convolved_image),
-                                  1 + np.max([np.max(self.image), np.max(self.convolved_image)]))
+                                  1 + self.max_imgs(self.image, self.convolved_image))
             self.update_text_annotation(text_annotation=self.conv_text, image=self.convolved_image)
 
     def max_imgs(self, img1, img2):
-        return np.max([np.max(img1), np.max(img2)])
+        return np.nanmax([np.nanmax(img1), np.nanmax(img2)])
 
     def min_imgs(self, img1, img2):
-        return np.min([np.min(img1), np.min(img2)])
+        return np.nanmin([np.nanmin(img1), np.nanmin(img2)])
 
 
     def convolve(self, change=None):
@@ -175,6 +195,14 @@ class KernelWidget():
         self.kernel = np.flipud(np.fliplr(self.kernel))
 
         self.convolved_image = ndimage.convolve(self.image, self.kernel, mode='constant', cval=0.)
+        if self.box_absolute.value == True:
+            self.convolved_image = np.abs(self.convolved_image)
+
+        if self.small_example:
+            self.convolved_image[0,:] = np.nan
+            self.convolved_image[-1,:] = np.nan
+            self.convolved_image[:,0] = np.nan
+            self.convolved_image[:,-1] = np.nan
         self.update_conv()
 
     def create_text_annotation(self, image, axes):
@@ -182,7 +210,11 @@ class KernelWidget():
         for i in range(image.shape[0]):
             text_annotation[i] = dict()
             for j in range(image.shape[1]):
-                text_annotation[i][j] = axes.text(j, i, image[i, j],
+                if np.isnan(image[i, j]):
+                    image_text = ''
+                else:
+                    image_text = str( image[i, j])
+                text_annotation[i][j] = axes.text(j, i, image_text,
                                ha="center", va="center", color="w", backgroundcolor=[0, 0, 0, 0.1])
 
         return text_annotation
@@ -190,4 +222,8 @@ class KernelWidget():
     def update_text_annotation(self, text_annotation, image):
         for i in range(image.shape[0]):
             for j in range(image.shape[1]):
-                text_annotation[i][j].set_text(str(image[i, j]))
+                if np.isnan(image[i, j]):
+                    image_text = ''
+                else:
+                    image_text = str( image[i, j])
+                text_annotation[i][j].set_text(image_text)
